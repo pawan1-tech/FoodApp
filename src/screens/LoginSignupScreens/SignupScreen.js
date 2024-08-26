6d96c34d6e35418a342dc70bd0b4c0456d9817da
Pawan Kumar Sah <120468294+pawan1-tech@users.noreply.github.com>
2024-08-26 11:49:39 +0530

    Sign in page created

---
+++ src/screens/LoginSignupScreens/SignupScreen.js
@@ -0,0 +1,11 @@
+import { View ,Text } from 'react-native'
+
+const SignupScreen = () => {
+  return (
+    <View>
+        <Text>Sign Up Screen</Text>
+    </View>
+  );
+}
+
+export default SignupScreen
